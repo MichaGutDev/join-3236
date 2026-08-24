7021694161ff2c819d052c67fb6d6abfecf6c443
Robert van den Boom <vdboom.robert@gmail.com>
2026-08-24 23:25:15 +0200

    feat: dataCreation [task] from form data

--- js/add_task.js
+++ js/add_task.js
@@ -36,21 +36,19 @@ const doneContainerRef = document.getElementById('done');
 
 const task = [
   {
-    toDo: {
-      id: 1,
-      title: "Create login page",
-      description: "Build the basic structure and styling for the login page.",
-      dueDate: "2026-08-25",
-      prio: "urgent",
-      category: "User Story",
-      assignedTo: ["contactId1", "contactId2"],
-      status: "To Do",
-      subtasks: [
-        { subtask: "Create HTML structure", completion: true },
-        { subtask: "Add responsive styling", completion: false },
-      ],
-    },
-  },
+    id: 1,
+    title: "Create login page",
+    description: "Build the basic structure and styling for the login page.",
+    dueDate: "2026-08-25",
+    prio: "urgent",
+    category: "User Story",
+    assignedTo: ["contactId1", "contactId2"],
+    status: "To Do",
+    subtasks: [
+      { subtask: "Create HTML structure", completion: true },
+      { subtask: "Add responsive styling", completion: false },
+    ],
+  }
 ];
 
 const formRef = document.querySelector('#todo-form');
@@ -62,8 +60,29 @@ formRef.addEventListener("submit", (event) => {
   getValues();
 });
 
-
 function getValues() {
   const formData = new FormData(formRef);
   console.log([...formData]);
+  dataCreation([...formData])
 };
+
+let valueLog = [];
+
+/**
+ * creates the task object based on the data given by getValues
+ * @param {array} formData - array of [key, value] from input submit
+ */
+function dataCreation(formData) {
+  const data = {
+    assignedTo: [],
+    subtasks: [],
+  };
+  formData.forEach(([key, value]) => {
+    if (key === "assignedTo" || key === "subtask") {
+      data[key].push(value);
+    } else {
+      data[key] = value;
+    }
+  });
+  valueLog.push(data);
+}
